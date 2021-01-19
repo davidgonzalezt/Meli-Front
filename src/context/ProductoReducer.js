5483d273e0c8c31561462e8eeb80aaef4b6931b9
davidgonzalezt <69219029+davidgonzalezt@users.noreply.github.com>
2021-01-19 14:18:04 -0300

    Success types added

--- src/context/ProductoReducer.js
+++ src/context/ProductoReducer.js
@@ -1,21 +1,34 @@
-import { GET_PRODUCTS, GET_PRODUCT } from "../types";
+import {
+  GET_PRODUCTS,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCT,
+  GET_PRODUCT_SUCCESS,
+} from "../types";
 
-const productosReducer  = (state, action)=>{
-    switch (action.type) {
-        case GET_PRODUCTS:
-            return{
-                ...state,
-                products: action.payload.items,
-                breadproducts: action.payload.bread
-            }
-        case GET_PRODUCT:
-            return{
-                ...state,
-                product: action.payload
-            }
-        default:
-            return state;
-    }
-}
+const productosReducer = (state, action) => {
+  switch (action.type) {
+    case GET_PRODUCT:
+    case GET_PRODUCTS:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        products: action.payload.items,
+        breadproducts: action.payload.bread,
+        loading: false,
+      };
+    case GET_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        product: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
 
 export default productosReducer;
